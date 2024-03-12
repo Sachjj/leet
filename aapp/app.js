6119dc20efb188a8b6353fc018cf24dd0abdd221
Sachjj <63959474+Sachjj@users.noreply.github.com>
2024-03-12 14:53:19 +0000

    hhhhhhh

---
+++ aapp/app.js
@@ -0,0 +1,63 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+
+//dislplay mobile menu
+
+const mobileMenuFunc = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenuFunc)
+
+const initSlider = () => {
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const sliderScrollbar = document.querySelector(".pic_container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    scrollbarThumb.addEventListener("mousedown", (e) => {
+        const startX = e.clientX; //horizontal mouse pointer coordinate
+
+        const handleMouseMove = (e) => {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = scrollbarThumb.offsetleft + deltaX;
+            scrollbarThumb.style.left = `${newThumbPosition}px`;
+        }
+
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        }
+
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    });
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({left:scrollAmount, behaviour: "smooth"});
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.color = imageList.scrollLeft <= 0 ? "grey" : "black"
+        slideButtons[1].style.color = imageList.scrollLeft >= maxScrollLeft ? "grey" : ""
+    }
+
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition/maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    }
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+        updateScrollThumbPosition();
+    })
+}
+
+window.addEventListener("load", initSlider)
